198fae8a595974f61b41d5f856009eb5478337ed
name <k>
2022-06-03 16:03:33 +0900

    0603

---
+++ javaPrjMVC/src/member/MemberController.java
@@ -0,0 +1,23 @@
+package member;
+
+public class MemberController {
+
+	public void join() {
+		System.out.println("===== 회원가입 =====");
+	}
+
+	public void login() {
+		System.out.println("===== 로그인 =====");
+	}
+
+	public void edit() {
+		System.out.println("===== 회원정보수정 =====");
+	}
+
+	public void quit() {
+		System.out.println("===== 탈퇴 =====");
+	}
+
+	}
+
+
